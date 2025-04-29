12dbef4814c8158a375f35f438bad4124cfab16b
RaelMorais <israelsantanamorais@outlook.com>
2025-04-29 11:34:45 -0300

    ad url

--- app/urls.py
+++ app/urls.py
@@ -9,7 +9,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('criar/', CreateUserView.as_view()),
-
+    path('listar/', ListUsuario.as_view()),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
